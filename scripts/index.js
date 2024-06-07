e6be871324a3fc9a56800af79f493dd20d9cf15d
Magomed Mutaliev <mutalievm350@gmail.com>
2024-06-07 14:20:36 +0300

    mesto-project-ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-
+function cardCreate(item, cardDelete) {
+  const card = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const cardBtnDelete = card.querySelector(".card__delete-button");
+  const cardImg = card.querySelector(".card__image");
+  cardImg.src = item.link;
+  cardImg.alt = item.name;
+  card.querySelector(".card__title").textContent = item.name;
+  cardBtnDelete.addEventListener("click", cardDelete);
+  return card;
+}
 // @todo: Функция удаления карточки
-
+function cardDelete(evt) {
+  evt.target.closest(".places__item").remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  const cardElement = cardCreate(item, cardDelete);
+  cardContainer.append(cardElement);
+});
